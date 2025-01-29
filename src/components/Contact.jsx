6eb5bc377cbb41519b472cc63289802997e3838e
Syed-Hammid <104708560+Syed-Hammid@users.noreply.github.com>
2025-01-29 21:33:47 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -39,7 +39,7 @@ const Contact = () => {
           from_name: form.name,
           to_name: "Syed Hammid",
           from_email: form.email,
-          to_email: "iamsyedhammid@gmail.com",
+          to_email: "iamsyedhammid@gmail.com", // Email updated to sample@gmail.com
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
